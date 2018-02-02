b78b42a6885d65e09ccc86482646c20bdb4e71b3
David Braley <david.braley@gmail.com>
2018-02-02 15:19:12 -0500

    React module compute cells update value when inputs are changed

--- react.js
+++ react.js
@@ -1,10 +1,16 @@
 class InputCell {
   constructor (value) {
+    this.computeCell = []
     this.setValue(value)
   }
 
   setValue (value) {
     this.value = value
+    this.computeCell.forEach(computeCell => computeCell.recompute())
+  }
+
+  setComputeCell (computeCell) {
+    this.computeCell.push(computeCell)
   }
 }
 
@@ -14,7 +20,14 @@ class CallbackCell {
 
 class ComputeCell {
   constructor (inputCellArray, cb) {
+    inputCellArray.forEach(inputCell => inputCell.setComputeCell(this))
+    this.inputCellArray = inputCellArray
+    this.cb = cb
     this.value = cb(inputCellArray)
+  }
+
+  recompute () {
+    this.value = this.cb(this.inputCellArray)
   }
 }
 
